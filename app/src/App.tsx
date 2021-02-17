3fd6b3f8c8d249c0e7e69a3144c65661e7ab2629
Luke <lukedturnbull@gmail.com>
2021-02-17 19:29:24 +1100

    Add Middleware and initial folder structure
    - Created Middleware for the react store
    - Created initial folder structure for components, redux, reducers and store
    - added combineReducer for future reducers
    - Changed typesscript jsx to "react" from "react-jsx"
    - Added types folder to add global interfaces/ types
    - Added initial lessons reducer and made the initial state the JSON from the challenge
    - Deleted logo
    - Created Lessons component to display name of list
    - General clean up

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import Lessons from "./components/Lessons";
+import { IAppProps } from "./type";
 
-function App() {
+function App(props: IAppProps) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={props.store}>
+      <Lessons />
+    </Provider>
   );
 }
 
