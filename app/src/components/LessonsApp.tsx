e6f7693b4d4e9c2f9d5d628e558878ccc17a9d8c
Luke <lukedturnbull@gmail.com>
2021-02-17 21:28:16 +1100

    Made parent LessonsApp

---
+++ app/src/components/LessonsApp.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import Lessons from "./Lessons";
+
+const LessonsApp: FC<{}> = () => {
+  return (
+    <div>
+      <Lessons />
+    </div>
+  );
+};
+
+export default LessonsApp;
